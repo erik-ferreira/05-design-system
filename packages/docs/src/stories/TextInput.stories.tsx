7d7d4af33b9bf0e655ce4a766c7a6f585a3402cd
erik-ferreira <erikdesouzagf@gmail.com>
2023-04-12 17:10:18 -0300

    create component TextInput; update annotation sb with decorators

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { TextInput, TextInputProps } from "@erik-ignite-ui/react";
+
+export default {
+  title: "Form/TextInput",
+  component: TextInput,
+  args: {},
+} as Meta<TextInputProps>;
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: "Type your name",
+  },
+};
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: "cal.com/",
+  },
+};
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+    placeholder: "Type your name",
+  },
+};
