9170e8f159e3e123d1ada67c61d7d9dbf5afd585
erik-ferreira <erikdesouzagf@gmail.com>
2023-04-13 17:34:59 -0300

    add argTypes in components sb

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: "https://github.com/erik-ferreira.png",
     alt: "Erik Ferreira",
   },
+  argTypes: {
+    src: {
+      control: {
+        type: "text",
+      },
+    },
+  },
 } as Meta<AvatarProps>;
 
 export const Primary: StoryObj<AvatarProps> = {};
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,6 +7,29 @@ export default {
   args: {
     children:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate atque labore officia reprehenderit dolor beatae est. Rerum vero, eos corporis est reprehenderit deserunt animi quae debitis temporibus, harum perspiciatis omnis.",
+    size: "md",
+  },
+  argTypes: {
+    size: {
+      options: [
+        "xxs",
+        "xs",
+        "sm",
+        "md",
+        "lg",
+        "xl",
+        "2xl",
+        "4xl",
+        "5xl",
+        "6xl",
+        "7xl",
+        "8xl",
+        "9xl",
+      ],
+      control: {
+        type: "inline-radio",
+      },
+    },
   },
 } as Meta<TextProps>;
 
