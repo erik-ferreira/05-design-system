301194c2d2d4416ad21d0f02a85057b9da5d9230
erik-ferreira <erikdesouzagf@gmail.com>
2023-04-14 18:08:29 -0300

    some fixes to project future

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,18 +1,21 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef, ElementRef } from "react";
 import { TextInputContainer, Prefix, Input } from "./styles";
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string;
 }
 
-export function TextInput({ prefix, ...rest }: TextInputProps) {
-  return (
-    <TextInputContainer>
-      {!!prefix && <Prefix>{prefix}</Prefix>}
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ prefix, ...rest }: TextInputProps, ref) => {
+    return (
+      <TextInputContainer>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
 
-      <Input {...rest} />
-    </TextInputContainer>
-  );
-}
+        <Input ref={ref} {...rest} />
+      </TextInputContainer>
+    );
+    // eslint-disable-next-line prettier/prettier
+  }
+);
 
 TextInput.displayName = "TextInput";
